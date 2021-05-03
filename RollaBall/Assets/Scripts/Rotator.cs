3edc8fb097f57b6dba5174f65ab6bd401b73ae70
sebastiaciudad <sebastia.ciudad@gmail.com>
2021-05-03 16:42:15 +0200

    Roll a Ball project
    Upload of the project roll a Ball

---
+++ RollaBall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private bool up = true;
+    private float speed = 0.5f;
+
+    private float StartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (up)
+        {
+            if (transform.position.y < (StartPosition + 0.25)){ transform.position = transform.position + (new Vector3(0, speed, 0) * Time.deltaTime); }
+            else { up = false; }
+        }
+        else
+        {
+            if (transform.position.y > (StartPosition - 0.25)) { transform.position = transform.position - (new Vector3(0, speed, 0) * Time.deltaTime); }
+            else { up = true; }
+        }
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
